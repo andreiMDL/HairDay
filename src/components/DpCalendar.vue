e75e539184a295ad6cb4a4869037dfd590684e66
andreiMDL <andreimoda8@gmail.com>
2025-08-26 23:02:39 -0300

    Add new SVG icons and implement custom date picker component
    - Added new SVG icons: white-calendar, white-cloud-sun, white-moon-stars, white-sun-horizon, white-user-square, yellow-calendar, and yellow-user-square.
    - Introduced a custom date picker component (DpCalendar.vue) using VueDatePicker.
    - Updated main.js to register the VueDatePicker component globally.
    - Replaced the default date input in SchedulingPage.vue with the new DpCalendar component.
    - Enhanced styling for the date input container and calendar icon.
    - Made minor adjustments to the main CSS file for better styling consistency.

---
+++ src/components/DpCalendar.vue
@@ -0,0 +1,54 @@
+<template>
+  <VueDatePicker v-model="date" class="custom-datepicker"></VueDatePicker>
+</template>
+<script>
+
+</script>
+<style>
+.custom-datepicker {
+  --dp-icon-color: var(--color-yellow);
+  --dp-background-color: var(--color-gray-700);
+  --dp-border-color: var(--color-gray-400);
+  --dp-border-radius: 2rem;
+
+  --dp-input-padding: 1rem 30px 6px 12px;
+  --dp-border-color-hover: var(--color-yellow);
+}
+
+.custom-datepicker .dp__input {
+  box-shadow: inset 0 0 10px black;
+}
+
+.custom-datepicker .dp__input:hover {
+  box-shadow: inset 0 0 10px black,
+    0 0 10px rgb(255, 174, 0);
+}
+
+.dp__menu {
+  background-color: var(--color-gray-600);
+  border: 1px solid var(--color-gray-800);
+  border-radius: 1rem;
+  box-shadow: 0 4px 10px rgba(0, 0, 0, 0.5);
+  /* color: #fff; */
+}
+
+.dp__today {
+  color: #fff;
+  border: none;
+  background-color: var(--color-gray-500);
+}
+
+.dp__active_date {
+  background-color: var(--color-gray-500);
+  border: 1px solid var(--color-yellow);
+  color: var(--color-yellow);
+}
+
+.dp__cell_offset {
+  color: var(--color-gray-400) !important;
+}
+
+.dp__arrow_top {
+  display: none;
+}
+</style>
--- src/views/SchedulingPage.vue
+++ src/views/SchedulingPage.vue
@@ -3,17 +3,24 @@
     <div class="schedule-container">
       <h2>Agende um atendimento</h2>
       <p>Selecione a data, horário e informe o nome do cliente para criar o agendamento</p>
-      <div class="date-container">
-        <input type="date" class="input-date">
+      <div class="date-input-container">
+        <DpCalendar />
       </div>
     </div>
   </div>
 </template>
-<script>
+<script setup>
+import DpCalendar from '@/components/DpCalendar.vue'
 </script>
 <style>
+body {
+  justify-content: center;
+  align-items: center;
+}
+
+
+
 .container {
-  /* border: 2px solid red; */
   width: 32rem;
   height: 55rem;
   border-radius: 2rem;
@@ -21,9 +28,65 @@
 }
 
 .schedule-container {
-  /* margin: 5rem; */
   padding: 5rem;
-  /* border: 1px solid blue; */
+}
 
+.date-container {
+  justify-content: space-between;
+  font-weight: 500;
+  position: relative;
+  display: flex;
+  align-items: center;
+  width: 100%;
+  padding: 12px 15px;
+  border-radius: 2rem;
+  background-color: var(--color-gray-600);
+  border: 1px solid var(--color-gray-400);
+  color: var(--color-gray-400);
+  box-shadow: inset 0 0 10px black;
+  padding: 12px 15px;
+  cursor: pointer;
+}
+
+.calendar-icon {
+  color: var(--color-primary-yellow);
+  margin-right: 10px;
+}
+
+.calendar-icon img {
+  vertical-align: middle;
+}
+
+.date-display {
+  flex-grow: 1;
+  color: #fff;
+  font-size: 16px;
+}
+
+.arrow-down {
+  color: var(--color-gray-400);
+  font-size: 8px;
+}
+
+.arrow-down img {
+  width: 1.3rem;
+  vertical-align: middle;
+}
+
+.hidden-date-input {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  opacity: 0;
+  cursor: pointer;
+}
+
+.date-input-container {
+  position: relative;
+  display: flex;
+  align-items: center;
+  width: 100%;
 }
 </style>
